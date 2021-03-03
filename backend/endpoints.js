22eac71f51728691bbc93936f483a3f25b2a85ed
gzirene <iguillen2@alu.ucam.edu>
2021-03-03 12:41:43 +0100

    Añadido servidor web

---
+++ backend/endpoints.js
@@ -0,0 +1,19 @@
+// Imports
+var router = require("express").Router();
+
+// Base endpoint
+router.get("/", (req, res) => {
+	res.send("Welcome to our api.")
+})
+router.get("/Hola", (req, res) => {
+	res.send("Hola.")
+})
+
+// Get a random number
+router.get("/random-number", (req, res) => {
+	let number = Math.floor(Math.random()*(100-1+1)+1)
+	res.json({number: number})
+})
+
+// Export the JS file
+module.exports = router;
